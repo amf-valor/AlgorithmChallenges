3aae17ad048888ddc488f0fdefed66d4b1075561
amf-valor <alan-1503@hotmail.com>
2020-02-14 16:11:29 -0300

    tape equilibrium solution

---
+++ Solutions/PermElemFinder.cs
@@ -0,0 +1,22 @@
+﻿namespace AlgorithmChallenges.Codility.Solutions
+{
+    public class PermElemFinder
+    {
+        public int Find(int[] A)
+        {
+            if (A.Length == 0) return 1;
+
+            long sumOfA = 0;
+
+            foreach (int item in A)
+            {
+                sumOfA += item;
+            }
+
+            // add length + 2 because the range can reach N+1
+            long fullSum = ((A.LongLength + 1) * (A.LongLength + 2)) / 2;
+
+            return (int) (fullSum - sumOfA);
+        }
+    }
+}
---
+++ Solutions/TapeEquilibriumMinimizer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgorithmChallenges.Codility.Solutions
+{
+    public class TapeEquilibriumMinimizer
+    {
+        public int Minimize(int[] A)
+        {
+            int rigthSum = A.Sum();
+            int leftSum = 0;
+            List<int> diffs = new List<int>();
+
+            for (int p = 1; p < A.Length; p++)
+            {
+                leftSum += A[p - 1];
+                rigthSum -= A[p - 1];
+                diffs.Add(Math.Abs(leftSum - rigthSum));
+            }
+
+            return diffs.Min();
+        }
+    }
+}
