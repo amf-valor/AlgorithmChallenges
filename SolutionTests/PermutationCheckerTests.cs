0f634864713ae2c21884e9edc7c870a3901a60c1
amf-valor <alan-1503@hotmail.com>
2020-03-05 15:42:08 -0300

    PermCheck solution

---
+++ SolutionTests/PermutationCheckerTests.cs
@@ -0,0 +1,69 @@
+﻿using AlgorithmChallenges.Codility.Solutions;
+using AlgorithmChallenges.Common;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AlgorithmChallenges.Codility.SolutionTests
+{
+    [TestClass]
+    public class PermutationCheckerTests
+    {
+        [TestMethod]
+        public void ShouldReturn1()
+        {
+            PermutationChecker checker = new PermutationChecker();
+            int actual = checker.Check(new int[] { 4, 1, 3, 2 });
+            int expected = 1;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldReturn0()
+        {
+            PermutationChecker checker = new PermutationChecker();
+            int actual = checker.Check(new int[] { 4, 1, 3});
+            int expected = 0;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldReturn1EvenWithOneElement()
+        {
+            PermutationChecker checker = new PermutationChecker();
+            int actual = checker.Check(new int[] { 1 });
+            int expected = 1;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldReturn0EvenWithOneElement()
+        {
+            PermutationChecker checker = new PermutationChecker();
+            int actual = checker.Check(new int[] { 3 });
+            int expected = 0;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldDelayLessThan150MilliSeconds()
+        {
+            int[] a = new int[100000];
+            Random randNum = new Random();
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = randNum.Next(1, 1000000000);
+            }
+
+
+            PermutationChecker checker = new PermutationChecker();
+            TimeSpan actual = Helpers.Measure(() => checker.Check(a));
+
+            Assert.IsTrue(actual < TimeSpan.FromSeconds(0.150));
+        }
+    }
+}
---
+++ Solutions/PermutationChecker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace AlgorithmChallenges.Codility.Solutions
+{
+    public class PermutationChecker
+    {
+        public int Check(int[] A)
+        {
+            HashSet<int> set = new HashSet<int>();
+
+            foreach(int item in A)
+            {
+                if (set.Contains(item))
+                {
+                    return 0;
+                }
+                else if (item <= A.Length)
+                {
+                    set.Add(item);
+                } 
+            }
+
+            return set.Count == A.Length ?  1 : 0;
+        }
+    }
+}
