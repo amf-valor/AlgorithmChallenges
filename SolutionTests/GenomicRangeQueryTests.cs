d6c73f5afaee4759bd63e4beee53e0ced8288cec
amf-valor <alan-1503@hotmail.com>
2020-03-11 18:23:00 -0300

    GenomicRangeQuery solution

---
+++ SolutionTests/GenomicRangeQueryTests.cs
@@ -0,0 +1,62 @@
+﻿using AlgorithmChallenges.Codility.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace AlgorithmChallenges.Codility.SolutionTests
+{
+    [TestClass]
+    public class GenomicRangeQueryTests
+    {
+        [TestMethod]
+        public void ShouldReturn241()
+        {
+            GenomicRangeQuery query = new GenomicRangeQuery();
+            int[] P = new int[] { 2, 5, 0 };
+            int[] Q = new int[] { 4, 5, 6 };
+            string dna = "CAGCCTA";
+            int[] expected = { 2,4,1 };
+            int[] actual = query.Query(dna, P, Q);
+
+            Assert.IsTrue(actual.SequenceEqual(expected));
+        }
+
+        [TestMethod]
+        public void ShouldReturn1()
+        {
+            GenomicRangeQuery query = new GenomicRangeQuery();
+            int[] P = new int[] { 0 };
+            int[] Q = new int[] { 0 };
+            string dna = "A";
+            int[] expected = { 1 };
+            int[] actual = query.Query(dna, P, Q);
+
+            Assert.IsTrue(actual.SequenceEqual(expected));
+        }
+
+        [TestMethod]
+        public void ShouldQueryAllRanges()
+        {
+            GenomicRangeQuery query = new GenomicRangeQuery();
+            int[] P = new int[] { 0, 1, 2, 3 };
+            int[] Q = new int[] { 0, 1, 2, 3 };
+            string dna = "ACGT";
+            int[] expected = { 1,2,3,4 };
+            int[] actual = query.Query(dna, P, Q);
+
+            Assert.IsTrue(actual.SequenceEqual(expected));
+        }
+
+        [TestMethod]
+        public void ShouldReturn112()
+        {
+            GenomicRangeQuery query = new GenomicRangeQuery();
+            int[] P = new int[] { 0, 0, 1 };
+            int[] Q = new int[] { 0, 1, 1 };
+            string dna = "AC";
+            int[] expected = { 1, 1, 2 };
+            int[] actual = query.Query(dna, P, Q);
+
+            Assert.IsTrue(actual.SequenceEqual(expected));
+        }
+    }
+}
---
+++ Solutions/GenomicRangeQuery.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmChallenges.Codility.Solutions
+{
+    public class GenomicRangeQuery
+    {
+        public int[] Query(string S, int[] P, int[] Q)
+        {
+            var factors = new Dictionary<char, int>()
+            {
+                {'A', 1},
+                {'C', 2},
+                {'G', 3},
+                {'T', 4}
+            };
+
+            var result = new int[P.Length];
+            var counters = new int[S.Length][];
+
+            //count all ocurrences of each factor
+            for (int i = 0; i < S.Length; i++)
+            {
+                counters[i] = new int[factors.Count];
+
+                if (i > 0)
+                {
+                    Array.Copy(counters[i - 1], counters[i], factors.Count);
+                }
+
+                foreach (var factor in factors)
+                {
+                    if (S[i] == factor.Key)
+                    {
+                        counters[i][factor.Value - 1]++;
+                        break;
+                    }
+                }
+            }
+
+            //compare the counters after prefix sums
+            for (int i = 0; i < P.Length; i++)
+            {
+                int from = P[i] - 1;
+
+                for (int j = 0; j < factors.Count; j++)
+                {
+                    //this evaluate corner cases like {0,0} {5,5} {0,1}
+                    //I mean queries which should compare on prefixed sum array
+                    int nucleotoideFrom = from >= 0 ? counters[from][j] : 0;
+                    int nucleotoideTo = counters[Q[i]][j];
+
+                    if (nucleotoideTo - nucleotoideFrom > 0)
+                    {
+                        result[i] = j + 1;
+                        break;
+                    }       
+                }
+            }
+
+            return result;
+        }
+    }
+}
