c4f0fd7c8d08d656918aab1662429ad538396de7
amf-valor <alan-1503@hotmail.com>
2020-03-12 18:20:47 -0300

    Distinct solution

---
+++ SolutionTests/DistinctGetterTests.cs
@@ -0,0 +1,70 @@
+﻿using AlgorithmChallenges.Codility.Solutions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmChallenges.Codility.SolutionTests
+{
+    [TestClass]
+    public class DistinctGetterTests
+    {
+        [TestMethod]
+        public void ShouldGet3DistinctNumbers()
+        {
+            DistinctGetter getter = new DistinctGetter();
+            int[] A = new int[] { 2,1,1,2,3,1 };
+            int expected = 3;
+            int actual = getter.Get(A);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldGet3DistinctNumbersWithNegatives()
+        {
+            DistinctGetter getter = new DistinctGetter();
+            int[] A = new int[] { -1, -1, -1000000, 1000000 };
+            int expected = 3;
+            int actual = getter.Get(A);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldGet0DistinctNumbersEmptyArray()
+        {
+            DistinctGetter getter = new DistinctGetter();
+            int[] A = new int[] { };
+            int expected = 0;
+            int actual = getter.Get(A);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldGetWithLambda3DistinctNumbers()
+        {
+            DistinctGetter getter = new DistinctGetter();
+            int[] A = new int[] { 2, 1, 1, 2, 3, 1 };
+            int expected = 3;
+            int actual = getter.GetWithLambda(A);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldGetWithLambda3DistinctNumbersWithNegatives()
+        {
+            DistinctGetter getter = new DistinctGetter();
+            int[] A = new int[] { -1, -1, -1000000, 1000000 };
+            int expected = 3;
+            int actual = getter.GetWithLambda(A);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldGetWithLambda0DistinctNumbersEmptyArray()
+        {
+            DistinctGetter getter = new DistinctGetter();
+            int[] A = new int[] { };
+            int expected = 0;
+            int actual = getter.GetWithLambda(A);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ Solutions/DistinctGetter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace AlgorithmChallenges.Codility.Solutions
+{
+    public class DistinctGetter
+    {
+        public int Get(int[] A)
+        {
+            if (A.Length == 0)
+                return 0;
+
+            int counter = 1;
+            Array.Sort(A);
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                int nextPosition = i + 1;
+
+                if (nextPosition < A.Length && A[i] != A[nextPosition])
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        public int GetWithLambda(int[] A)
+        {
+            return A.Distinct().Count();
+        }
+    }
+}
