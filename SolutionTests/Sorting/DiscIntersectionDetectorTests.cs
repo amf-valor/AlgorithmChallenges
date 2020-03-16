273a4ed6491c4d026f1faa334489ec71ad28da9e
amf-valor <alan-1503@hotmail.com>
2020-03-16 19:39:37 -0300

    NumberOfDisIntersections solution

---
+++ SolutionTests/Sorting/DiscIntersectionDetectorTests.cs
@@ -0,0 +1,19 @@
+﻿using AlgorithmChallenges.Codility.Solutions.Sorting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmChallenges.Codility.SolutionTests.Sorting
+{
+    [TestClass]
+    public class DiscIntersectionDetectorTests
+    {
+        [DataRow(new int[] { 1, 5, 2, 1, 4, 0 }, 11)]
+        [DataRow(new int[] { 1, 2147483647, 0 }, 2)]
+        [DataTestMethod]
+        public void ShouldDetectIntersections(int[] A, int expected)
+        {
+            DiscIntersectionsDetector detector = new DiscIntersectionsDetector();
+            int actual = detector.Detect(A);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
