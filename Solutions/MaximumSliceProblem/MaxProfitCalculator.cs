062f623f8c4ef967b1e2b3020e2d04620b92581e
amf-valor <alan-1503@hotmail.com>
2020-03-23 20:13:46 -0300

    MaxProfit solution

---
+++ Solutions/MaximumSliceProblem/MaxProfitCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AlgorithmChallenges.Codility.Solutions.MaximumSliceProblem
+{
+    /// <summary>
+    /// Lesson 9 - MaxProfit
+    /// </summary>
+    public class MaxProfitCalculator
+    {
+        public int Calculate(int[] A)
+        {
+            int maxProfit = 0;
+            int minPrice = 200000;
+            
+            foreach (int price in A)
+            {
+                minPrice = Math.Min(price, minPrice);
+                maxProfit = Math.Max(maxProfit, price - minPrice);
+            }
+
+            return maxProfit;
+        }
+    }
+}
