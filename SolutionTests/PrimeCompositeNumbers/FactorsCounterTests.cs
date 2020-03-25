cf0c37bee22ef14d010e88969938d222f32d8afb
amf-valor <alan-1503@hotmail.com>
2020-03-25 18:18:13 -0300

    Count Factors solution

---
+++ SolutionTests/PrimeCompositeNumbers/FactorsCounterTests.cs
@@ -0,0 +1,22 @@
+﻿using AlgorithmChallenges.Codility.Solutions.PrimeCompositeNumbers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmChallenges.Codility.SolutionTests.PrimeCompositeNumbers
+{
+    [TestClass]
+    public class FactorsCounterTests
+    {
+        [TestMethod]
+        [DataRow(24, 8, DisplayName = "example test")]
+        [DataRow(7, 2, DisplayName = "ShouldCount2ForPrimes")]
+        [DataRow(1, 1, DisplayName = "ShouldCount1For1Prime")]
+        [DataRow(41, 2, DisplayName = "ShouldCount2ForPrime41")]
+        [DataRow(16, 5, DisplayName = "ShouldCount5ForCompositeSquareEqualsN")]
+        public void ShouldCountFactorsOfN(int N, int expected)
+        {
+            FactorsCounter counter = new FactorsCounter();
+            int actual = counter.Count(N);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
