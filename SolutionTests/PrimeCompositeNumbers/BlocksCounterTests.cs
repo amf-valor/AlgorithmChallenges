09a10daa23d9a3a2cae30c05c69ab826e1c865eb
amf-valor <alan-1503@hotmail.com>
2020-03-28 09:49:17 -0300

    Peaks solution

---
+++ SolutionTests/PrimeCompositeNumbers/BlocksCounterTests.cs
@@ -0,0 +1,25 @@
+﻿using AlgorithmChallenges.Codility.Solutions.PrimeCompositeNumbers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmChallenges.Codility.SolutionTests.PrimeCompositeNumbers
+{
+    [TestClass]
+    public class BlocksCounterTests
+    {
+        [DataTestMethod]
+        [DataRow(new int[] { 1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2 }, 3, DisplayName = "ExampleTest")]
+        [DataRow(new int[] { 1, 2, 3, 4 }, 0, DisplayName = "ShouldCount0BlocksWhenNoPeakIsFound")]
+        [DataRow(new int[] { 1 }, 0, DisplayName = "ShouldCount0BlocksForArrayOf1Element")]
+        [DataRow(new int[] { 1, 2 }, 0, DisplayName = "ShouldCount0BlocksForArrayOf2Element")]
+        [DataRow(new int[] { 1, 2, 1, 4, 5, 6, 7, 8, 9, 10, 9, 12 }, 2, DisplayName = "ShouldCount2BlocksFor2Peaks")]
+        [DataRow(new int[] { 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 }, 4, DisplayName = "ShouldCount4BlocksForMaximumPossiblePeaks")]
+        [DataRow(new int[] { 1, 2, 1, 2, 1, 2, 1, 2, 3, 4, 5, 6 }, 1, DisplayName = "ShouldCount1BlockFor3PeaksOnlyInTheBegining")]
+        [DataRow(new int[] { 1, 2, 1, 2, 1 }, 1, DisplayName = "ShouldCount1BlockForPrimeElements")]
+        public void ShouldCountBlocksOfA(int[] A, int expected)
+        {
+            BlocksCounter counter = new BlocksCounter();
+            int actual = counter.Count(A);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Solutions/PrimeCompositeNumbers/BlocksCounter.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace AlgorithmChallenges.Codility.Solutions.PrimeCompositeNumbers
+{
+    /// <summary>
+    /// Lesson 10 - Peaks
+    /// </summary>
+    public class BlocksCounter
+    {
+        /// <summary>
+        /// First we count the peaks and record the indexes 
+        /// Second test smaller size of a block to the bigger, because how smaller is a block
+        /// more blocks it can be divided. 
+        /// Third Testing the blocks we divide each peakIndex by the block number if the result
+        /// is equal it means the peak is inside of this block so we increment the block number and check.
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public int Count(int[] A)
+        {
+            int N = A.Length;
+            int blocks = 0;
+            var peaks = new List<int>();
+
+            if (N < 3)
+            {
+                return blocks;
+            }
+
+            for (int i = 1; i < N - 1; i++)
+            {
+                if (A[i - 1] < A[i] && A[i] > A[i + 1])
+                {
+                    peaks.Add(i);
+                }
+            }
+
+            if (peaks.Count > 0)
+            {
+                blocks++;
+
+                for (int blocksCount = N - 1; blocksCount > 1; blocksCount--)
+                {
+                    if (N % blocksCount != 0)
+                    {
+                        continue;
+                    }
+
+                    int blockSize = N / blocksCount;
+                    int blocksHasPeaks = 0;
+
+                    foreach (int peak in peaks)
+                    {
+                        if (peak / blockSize == blocksHasPeaks)
+                        {
+                            blocksHasPeaks++;
+                        }
+                    }
+
+                    if(blocksHasPeaks == blocksCount)
+                    {
+                        blocks = blocksCount;
+                        break;
+                    }
+                } 
+            }
+
+            return blocks;
+        }
+    }
+}
