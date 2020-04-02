65a12b9475cf936556295cfadd7e8d6a41376267
amf-valor <alan-1503@hotmail.com>
2020-04-02 10:28:27 -0300

    common prime divisors solution

---
+++ SolutionTests/EuclideanAlgorithm/CommonPrimeDivisorsCounterTests.cs
@@ -0,0 +1,19 @@
+﻿using AlgorithmChallenges.Codility.Solutions.EuclideanAlgorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmChallenges.Codility.SolutionTests.EuclideanAlgorithm
+{
+    [TestClass]
+    public class CommonPrimeDivisorsCounterTests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 17, 7}, new int[] { 23, 13}, 0, DisplayName ="OnlyPrimes")]
+        [DataRow(new int[] { 15, 10, 3 }, new int[] { 75, 30, 5 }, 1, DisplayName ="ExampleTests")]
+        public void ShouldCount_TwoNonEmptyArrays_ReturnsCounterOfCommonPrimesDivisor(int[]A, int[]B, int expected)
+        {
+            var counter = new CommonPrimeDivisorsCounter();
+            var actual = counter.Count(A, B);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
