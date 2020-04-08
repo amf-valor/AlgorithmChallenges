4f5532c5be81b1c82a8e74c8d63cabdc7c715788
amf-valor <alan-1503@hotmail.com>
2020-04-08 19:54:20 -0300

    MinMaxDivision solution

---
+++ Solutions/BinarySearchAlgorithm/LargeSumMinimizor.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+
+namespace AlgorithmChallenges.Codility.Solutions.BinarySearchAlgorithm
+{
+    /// <summary>
+    /// Lesson 14 - Binary Search Algorithm
+    /// </summary>
+    public class LargeSumMinimizor
+    {
+        /// <summary>
+        /// we use binary search with predicate evaluation(greedy checking), the link bellow is a really good reference of explanation
+        /// https://www.topcoder.com/community/competitive-programming/tutorials/binary-search/
+        /// </summary>
+        /// <param name="K"></param>
+        /// <param name="M"></param>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public int Minimize(int K, int M, int[] A) 
+        {
+            int maxSum = A.Sum();
+            int minSum = A.Max();
+            int minLargeSum = 0;
+
+            if (K == 1) 
+            {
+                return maxSum;
+            }
+
+            if (K >= A.Length) 
+            {
+                return minSum;
+            }
+
+            while (minSum <= maxSum) 
+            {
+                int middleSum = (minSum + maxSum) / 2;
+
+                if (HasEnoughBlocks(A, middleSum, K))
+                {
+                    minLargeSum = middleSum;
+                    maxSum = middleSum - 1;
+                }
+                else 
+                {
+                    minSum = middleSum + 1;
+                }
+            }
+
+            return minLargeSum;
+        }
+
+        private bool HasEnoughBlocks(int[] a, int middleSum, int k)
+        {
+            int blocks = k;
+            int largeSum = 0;
+
+            for (int i = 0; i < a.Length; i++) 
+            {
+                largeSum += a[i];
+
+                if(largeSum > middleSum) 
+                {
+                    blocks--;
+                    largeSum = a[i];
+                }
+
+                if(blocks == 0) 
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
