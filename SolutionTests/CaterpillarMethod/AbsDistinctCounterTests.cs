92859bb9be04f7ff183e9349d0ac257f27e0a716
amf-valor <alan-1503@hotmail.com>
2020-04-10 10:38:06 -0300

    AbsDistinct solution

---
+++ SolutionTests/CaterpillarMethod/AbsDistinctCounterTests.cs
@@ -0,0 +1,26 @@
+﻿using AlgorithmChallenges.Codility.Solutions.CaterpillarMethod;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmChallenges.Codility.SolutionTests.CaterpillarMethod
+{
+    [TestClass]
+    public class AbsDistinctCounterTests
+    {
+        [TestMethod]
+        [DataRow(new int[] { -5, 3, -1, 0, 3, 6 }, 5, DisplayName = "CountValuesShouldReturn5")]
+        [DataRow(new int[] { -9, -3, -3, 2, 3, 3, 3 }, 3, DisplayName = "CountValuesShouldReturn3")]
+        [DataRow(new int[] { -3, -3, 3, 3, 3 }, 1, DisplayName = "CountAllSameAbsValuesShouldReturn1")]
+        [DataRow(new int[] { -3, -3, -2, -2, 2, 3, 3, 3 }, 2, DisplayName = "Count2DeacreasingAbsValuesShouldReturn2")]
+        [DataRow(new int[] { -3 }, 1, DisplayName = "CountSingleValueShouldReturn1")]
+        [DataRow(new int[] { int.MinValue, int.MinValue, int.MaxValue, int.MaxValue }, 2, DisplayName = "CountExtremeValuesShouldReturn2")]
+        [DataRow(new int[] { int.MinValue, int.MinValue, int.MinValue }, 1, DisplayName = "CountAllMinValuesShouldReturn1")]
+        [DataRow(new int[] { int.MinValue, -1, 0, 1}, 3, DisplayName = "CountMinAndRandomValuesShouldReturn3")]
+        public void CountValuesShouldReturnDistinctAbsoluteOnes(int[] a, int expected)
+        {
+            AbsDistinctCounter counter = new AbsDistinctCounter();
+            int actual = counter.Count(a);
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
