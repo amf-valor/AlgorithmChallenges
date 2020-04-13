68701a5dea25a7f0be9e4c2e02c785530fbdc83d
amf-valor <alan-1503@hotmail.com>
2020-04-13 12:42:39 -0300

    upgrade to .net core 3.1 and NumberSolitaire solution

---
+++ SolutionTests/DynamicPrograming/MaximalSumFinderTests.cs
@@ -0,0 +1,18 @@
+﻿using AlgorithmChallenges.Codility.Solutions.DynamicPrograming;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AlgorithmChallenges.Codility.SolutionTests.DynamicPrograming
+{
+    [TestClass]
+    public class MaximalSumFinderTests
+    {
+        [TestMethod]
+        [DataRow(new int[] { 1, -2, 0, 9, -1, -2 }, 8)]
+        public void FindShouldReturnMaximalSum(int[] a, int expected)
+        {
+            MaximalSumFinder finder = new MaximalSumFinder();
+            int actual = finder.Find(a);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Solutions/DynamicPrograming/MaximalSumFinder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AlgorithmChallenges.Codility.Solutions.DynamicPrograming
+{
+    /// <summary>
+    /// Lesson 15 - Number Solitaire
+    /// </summary>
+    public class MaximalSumFinder
+    {
+        public int Find(int[] A)
+        {
+            int[] maxs = new int[A.Length];
+            maxs[0] = A[0];
+
+            for (int i = 1; i < A.Length; i++)
+            {
+                maxs[i] = maxs[i - 1];
+                 
+                for (int diceFace = 2; diceFace <= 6; diceFace++)
+                {
+                    if (i >= diceFace)
+                    {
+                        maxs[i] = Math.Max(maxs[i], maxs[i - diceFace]);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                maxs[i] += A[i];
+            }
+
+            return maxs[A.Length - 1];
+        }
+    }
+}
