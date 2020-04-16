3369174ac4356e39e6d90d24c8e01c297a2dd133
amf-valor <alan-1503@hotmail.com>
2020-04-16 16:38:30 -0300

    CtCi ArrayAndStrings CheckPermutation solution

---
+++ CrackingTheCodeInterview/CtCi.SolutionTests/ArraysAndStrings/CheckPermutationTests.cs
@@ -0,0 +1,26 @@
+﻿using CtCi.Solutions.ArraysAndStrings;
+using Xunit;
+
+namespace CtCi.SolutionTests.ArraysAndStrings
+{
+    public class CheckPermutationTests
+    {
+        [Theory]
+        [InlineData("aaa", null, false)]
+        [InlineData(null, "bbb", false)]
+        [InlineData(null, null, false)]
+        [InlineData("aaa", "", false)]
+        [InlineData("", "bbb", false)]
+        [InlineData("", "", false)]
+        [InlineData("aaa", "bbb", false)]
+        [InlineData("abc", "bca", true)]
+        [InlineData("aaa", "bb", false)]
+        [InlineData("bba", "bbb", false)]
+        public void CheckPermutationShoulReturnTrueOrFalseWhenGivenOneAndOtherStrings(string one, string other, bool expected) 
+        {
+            CheckPermutation checkPermutation = new CheckPermutation();
+            bool actual = checkPermutation.IsPermutation(one, other);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ CrackingTheCodeInterview/CtCi.Solutions/ArraysAndStrings/CheckPermutation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace CtCi.Solutions.ArraysAndStrings
+{
+    public class CheckPermutation
+    {
+        /// <summary>
+        /// Remmember to ask for assumptions like strings are unicode, ASCII, is case sensitive
+        /// </summary>
+        /// <param name="one"></param>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool IsPermutation(string one, string other) 
+        {
+            if (string.IsNullOrEmpty(one) || 
+                string.IsNullOrEmpty(other) ||
+                one.Length != other.Length) 
+            {
+                return false;
+            }
+
+            HashSet<char> charHashSet = new HashSet<char>(other.ToCharArray());
+
+            foreach (char c in one) 
+            {
+                if (!charHashSet.Contains(c)) 
+                {
+                    return false ;
+                }
+            }
+
+            return true;
+        }
+    }
+}
