0dca5fc2657e6fe29974b3e28f307cfe54570e75
amf-valor <alan-1503@hotmail.com>
2020-04-18 10:16:06 -0300

    CtCi RotateMatrix solution

---
+++ CrackingTheCodeInterview/CtCi.SolutionTests/ArraysAndStrings/RotateMatrixTests.cs
@@ -0,0 +1,41 @@
+﻿using CtCi.Solutions.ArraysAndStrings;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CtCi.SolutionTests.ArraysAndStrings
+{
+    public class RotateMatrixTests
+    {
+        [Theory]
+        [MemberData(nameof(Rotate90DegreeTestData))]
+        public void Rotate90DegreeShouldRotateGivenMatrix(int[][] squareMatrix, int[][] expected)
+        {
+            RotateMatrix rotate = new RotateMatrix(squareMatrix);
+            int[][] actual = rotate.Rotate90Degree();
+            Assert.Equal(expected, actual);
+        }
+
+        public static IEnumerable<object[]> Rotate90DegreeTestData =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                    new int[][]
+                    {
+                        new int []{ 1, 2, 3, 4 },
+                        new int []{ 5, 6, 7, 8},
+                        new int []{ 9, 10, 11, 12 },
+                        new int []{ 31, 10, 15, 200},
+                    },
+                    new int [][]
+                    { 
+                        new int []{ 4, 8, 12, 200 },
+                        new int []{ 3, 7, 11, 15 },
+                        new int []{ 2, 6 ,10, 10 },
+                        new int []{ 1, 5, 9, 31 }
+                    }
+                }
+            };
+    }
+}
---
+++ CrackingTheCodeInterview/CtCi.Solutions/ArraysAndStrings/RotateMatrix.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace CtCi.Solutions.ArraysAndStrings
+{
+    public class RotateMatrix
+    {
+        private readonly int[][] _squareMatrix;
+
+        public RotateMatrix(int[][] squareMatrix)
+        {
+            if (squareMatrix.Length == 0 || squareMatrix == null)
+            {
+                throw new ArgumentException("matrix is not in NxN format");
+            }
+
+            _squareMatrix = squareMatrix;
+        }
+
+        public int[][] Rotate90Degree()
+        {
+            Layer layer = new Layer(_squareMatrix.Length - 1);
+            
+            while (layer.Row < _squareMatrix.Length / 2)
+            {
+                while (layer.Column < layer.Limit)
+                {
+                    int top = GetTop(layer);
+                    SwapRightToTop(layer);
+                    SwapBottomToRight(layer);
+                    SwapLeftToBottom(layer);
+                    PutTopOnBottom(layer, top);
+                    layer.MoveToNextColumn();
+                }
+
+                layer.MoveNext();
+            }
+
+            return _squareMatrix;
+        }
+        private void SwapRightToTop(Layer layer)
+        {
+            _squareMatrix[layer.Row][layer.Column] = _squareMatrix[layer.Row + layer.Offset][layer.Limit];
+        }
+        private void SwapBottomToRight(Layer layer)
+        {
+            _squareMatrix[layer.Row + layer.Offset][layer.Limit] = _squareMatrix[layer.Limit][layer.Limit - layer.Offset];
+        }
+        private void SwapLeftToBottom(Layer layer)
+        {
+            _squareMatrix[layer.Limit][layer.Limit - layer.Offset] = _squareMatrix[layer.Limit - layer.Offset][layer.Row];
+        }
+
+        private void PutTopOnBottom(Layer layer, int top)
+        {
+            _squareMatrix[layer.Limit - layer.Offset][layer.Row] = top;
+        }
+
+        private int GetTop(Layer layer)
+        {
+            return _squareMatrix[layer.Row][layer.Column];
+        }
+        //TODO: layer could implement IEnumerable pattern
+        private class Layer 
+        {
+            public int Row { get; private set; }
+            public int Limit { get; private set; }
+            public int Offset 
+            {
+                get { return Column - Row; }  
+            }
+
+            public int Column { get; private set; }
+
+            public Layer(int limit)
+            {
+                Limit = limit;
+            }
+
+            public void MoveNext() 
+            {
+                Row++;
+                Limit--;
+                Column = Row;
+            }
+
+            public void MoveToNextColumn() 
+            {
+                Column++;
+            }
+        }
+    }
+}
