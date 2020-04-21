d8dd3aa6ccc25fda9bb95588af44a285ddfc331a
amf-valor <alan-1503@hotmail.com>
2020-04-21 20:09:44 -0300

    CtCi LinkedLists Partition solution

---
+++ CrackingTheCodeInterview/CtCi.SolutionTests/LinkedLists/PartitionTest.cs
@@ -0,0 +1,59 @@
+﻿using CtCi.Solutions.LinkedLists;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CtCi.SolutionTests.LinkedLists
+{
+    public class PartitionTest
+    {
+        [Theory]
+        [MemberData(nameof(PartionedLinkedListAroundXTestData))]
+        public void ToPartitionShouldReturnPartionedLinkedListAroundXWhenGivenXandALinkedList(int x, LinkedList<int> aLinkedList, LinkedList<int> expected) 
+        {
+            Partition partitioner = new Partition();
+            LinkedList<int> actual = partitioner.ToPartition(x, aLinkedList);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(ThrowsArgumentExceptionTestData))]
+        public void ToPartitionShouldThrowsArgumentExceptionWhenGivenALinkedListIsNullOrEmpty(int x, LinkedList<int> aLinkedList) 
+        {
+            Partition partioner = new Partition();
+            Func<int, LinkedList<int>, LinkedList<int>> actual = partioner.ToPartition;
+            Assert.Throws<ArgumentException>(() => actual(x, aLinkedList));
+        }
+
+        public static IEnumerable<object[]> PartionedLinkedListAroundXTestData =>
+            new List<object[]>
+            {
+                new object[]
+                {
+                  5,
+                  new LinkedList<int>(new int[]{ 3, 5, 8, 5, 10, 2, 1 }),
+                  new LinkedList<int>(new int[]{ 1, 2, 3, 5, 8, 5, 10 })
+                },
+                new object[]
+                {
+                  10,
+                  new LinkedList<int>(new int[]{ 5, 30, 6, 15}),
+                  new LinkedList<int>(new int[]{ 6, 5, 30, 15})
+                }
+
+            };
+
+        public static IEnumerable<object[]> ThrowsArgumentExceptionTestData =>
+            new List<object[]>
+            {
+                new object[]
+                { 
+                    0, null
+                },
+                new object[]
+                { 
+                    1, new LinkedList<int>()
+                }
+            };
+    }
+}
---
+++ CrackingTheCodeInterview/CtCi.Solutions/LinkedLists/Partition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CtCi.Solutions.LinkedLists
+{
+    public class Partition
+    {
+        public LinkedList<int> ToPartition(int x, LinkedList<int> aLinkedList) 
+        {
+            if (aLinkedList is null || aLinkedList.Count == 0) 
+            {
+                throw new ArgumentException(" aLinkedList cannot be null or empty");
+            }
+
+            LinkedList<int> partioned = new LinkedList<int>();
+
+            foreach (int item in aLinkedList) 
+            {
+                if (item < x)
+                {
+                    partioned.AddFirst(item);
+                }
+                else 
+                {
+                    partioned.AddLast(item);
+                }
+            }
+
+            return partioned;
+        }
+    }
+}
