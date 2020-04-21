d287cb155715e1d34ac60a61017d6944e714a000
amf-valor <alan-1503@hotmail.com>
2020-04-21 18:13:36 -0300

    CtCi RemoveDups solution

---
+++ CrackingTheCodeInterview/CtCi.SolutionTests/LinkedLists/RemoveDupsTests.cs
@@ -0,0 +1,58 @@
+﻿using CtCi.Solutions.LinkedLists;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace CtCi.SolutionTests.LinkedLists
+{
+    public class RemoveDupsTests
+    {
+        [Theory]
+        [MemberData(nameof(RemoveDuplicatedValuesTestData))]
+        public void DistinctShouldRemoveDuplicatedValuesWhenGivenALinkedList(LinkedList<int> actual, LinkedList<int> expected) 
+        {
+            actual.Distinct();
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [MemberData(nameof(ShouldThrowArgumentExceptionTestData))]
+        public void DistinctShouldThrowArgumentExceptionWhenLinkedListIsNullOrEmpty(LinkedList<int> actual) 
+        {
+            Assert.Throws<ArgumentException>(() => actual.Distinct());
+        }
+
+        public static IEnumerable<object[]> RemoveDuplicatedValuesTestData =>
+            new List<object[]> 
+            {
+                new object[]
+                {
+                    new LinkedList<int>(new int[] { 1, 1, 2, 2 }),
+                    new LinkedList<int>(new int[] { 1, 2 })
+                },
+                new object[]
+                {
+                    new LinkedList<int>(new int[] { 1, 2, 1, 3 }),
+                    new LinkedList<int>(new int[] { 1, 2, 3 })
+                },
+                new object[]
+                {
+                    new LinkedList<int>(new int[] { 1, 2, 1, 3, 2, 3, 3 }),
+                    new LinkedList<int>(new int[] { 1, 2, 3 })
+                }
+            };
+
+        public static IEnumerable<object[]> ShouldThrowArgumentExceptionTestData =>
+            new List<object[]> 
+            { 
+                new object[]
+                {
+                    new LinkedList<int>(new int[]{ }),
+                },
+                new object[]
+                {
+                    null,
+                }
+            };
+    }
+}
---
+++ CrackingTheCodeInterview/CtCi.Solutions/LinkedLists/RemoveDups.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CtCi.Solutions.LinkedLists
+{
+    public static class RemoveDups
+    {
+        /// <summary>
+        /// Because I am using .Net Core Implementation of LinkedList the Next and Previous of each node are read-only
+        /// So I need to use the Remove method which is an O(n) operation becoming this algorithm O(n2) instead of O(n) 
+        /// </summary>
+        /// <param name="linkedList"></param>
+        public static void Distinct(this LinkedList<int> linkedList)
+        {
+            if (linkedList is null || linkedList.Count == 0) 
+            {
+                throw new ArgumentException("LinkedList cannot be null or empty");
+            }
+
+            LinkedListNode<int> node = linkedList.First;
+            bool[] marks = new bool[linkedList.Count];
+
+            while (node != null) 
+            {
+                if (marks[node.Value])
+                {
+                    LinkedListNode<int> next = node.Next;
+                    linkedList.Remove(node);
+                    node = next;
+                }
+                else 
+                {
+                    marks[node.Value] = true;
+                    node = node.Next;
+                }
+            }
+        }
+    }
+}
